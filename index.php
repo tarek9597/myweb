8370385b6d0cb0dea922fc24b821643cc82f2744
unknown <TARIQUL@WINCTRL-AC03VDM.(none)>
2014-08-15 01:07:25 +0600

    fff

--- index.php
+++ index.php
@@ -10,51 +10,20 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        
-        <form>
-            
-            Number 1: <input type="text" name="number1"><br/> <p/>
-            Number 2: <input type="text" name="number2"><br/>
-            <p/>
-            <input type="submit" name="add" value="Add">
-            <input type="submit" name="substruct" value="Substruct">
-            <input type="submit" name="multiply" value="Multiply">
-            <input type="submit" name="divide" value="Divide"><br/> <p/>
-                
-        </form>
-        
+
+
         <?php
-        echo "Result :"  ;
-        $num1=$_GET["number1"];
-        $num2=$_GET["number2"];
-      //  $submitAdd=$_GET["add"]; 
-        //$submitSubstruct=$_GET["substruct"]; 
-        //$submitMultiply=$_GET["multiply"];
-        //$submitDivide=$_GET["divide"];
-        
-         if (isset($num1) && isset($num2)&& isset($_GET["add"]))
-            {
-            $add=$num1+$num2;
-            echo " $add";
-            }
-        
-    else if (isset($num1) && isset($num2)&& isset($_GET["substruct"]))
-            {
-            $substruct=$num1-$num2;
-            echo " $substruct";
-            }
-           
-    else if (isset($num1) && isset($num2)&& isset($_GET["multiply"]))
-            {
-            $multiply=$num1*$num2;
-            echo " $multiply";
-            }
-            
-    else if (isset($num1) && isset($num2)&& isset($_GET["divide"]))
-            {
-            $divide=$num1/$num2;
-            echo " $divide";
-            }  
+        $numArray = array(38, 2, 9, 5, 49, 35, 20, 50, 25, 53);
+
+
+        for ($i = 0; $i < count($numArray); $i++) {
+
+            sort($numArray);
+            echo($numArray [$i]) . " ";
+        }
+
+        echo "<p/>";
         ?>
+        
     </body>
 </html>
